07db00635b4c79fb7c05920edcebc58720d9c5a6
Your Name <you@example.com>
2023-02-06 10:08:02 +0700

    Tree Post Order

---
+++ TestBTS.java
@@ -0,0 +1,17 @@
+class TestBST {
+    public static void main(String[] args) {
+        //create a BST
+        BST<String> tree = new BST<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        //traverse tree
+        System.out.println("Postorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
